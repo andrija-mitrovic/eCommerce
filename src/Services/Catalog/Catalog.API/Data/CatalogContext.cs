7351e423a750ef3f7bc0e9d87edd6635af23c9cd
Andrija Mitrovic <andrija.mitrovic@bild-studio.net>
2021-12-04 10:09:52 +0100

    Catalog context added

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration[Constants.MongoDbConnectionString]);
+            var database = client.GetDatabase(configuration[Constants.MongoDbDatabaseConnectionString]);
+
+            Products = database.GetCollection<Product>(configuration[Constants.MongoDbConnectionName]);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}

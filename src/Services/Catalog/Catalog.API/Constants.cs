e1a0440bed16d05155e93a994c6f2bd1e8ab7529
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-12-07 08:48:32 +0100

    Constants class added in Catalog.API project for storing global constants

---
+++ src/Services/Catalog/Catalog.API/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API
+{
+    public class Constants
+    {
+        public const string MongoDbConnectionString = "DatabaseSettings:ConnectionString";
+        public const string MongoDbDatabaseConnectionString = "DatabaseSettings:DatabaseName";
+        public const string MongoDbConnectionName = "DatabaseSettings:CollectionName";
+    }
+}

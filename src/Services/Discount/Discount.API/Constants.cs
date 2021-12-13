48d3713abddd29c5c216a2ed14ad7d47320ee049
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-12-13 08:35:42 +0100

    Discount.API project added

---
+++ src/Services/Discount/Discount.API/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API
+{
+    public class Constants
+    {
+        public const string PostgresqlDatabaseConnection = "DatabaseSettings:ConnectionString";
+    }
+}

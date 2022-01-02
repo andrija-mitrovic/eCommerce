ccb981d9a531dae1905c4abd691733e3764e6aac
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-02 09:24:55 +0100

    Added class for storing constants in infrastructure project

---
+++ src/Services/Ordering/Ordering.Infrastructure/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Infrastructure
+{
+    public class Constants
+    {
+        public const string OrderingConnection = "OrderingConnectionString";
+        public const string EmailSettings = "EmailSettings";
+    }
+}

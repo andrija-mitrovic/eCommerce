4d3bd893f406ada1be2c0ef0126fb13fd9a22c13
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-12 08:56:07 +0100

    Constant class for storing variables added

---
+++ src/ApiGateways/Shopping.Aggregator/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Aggregator
+{
+    public class Constants
+    {
+        public const string CatalogUrl = "ApiSettings:CatalogUrl";
+        public const string BasketUrl = "ApiSettings:BasketUrl";
+        public const string OrderingUrl = "ApiSettings:OrderingUrl";
+        public const string GetCatalog = "/api/v1/Catalog/";
+        public const string GetCatalogByCategory = "/api/v1/Catalog/GetProductByCategory/";
+        public const string GetBasket = "/api/v1/Basket/";
+        public const string GetOrdersByUserName = "/api/v1/Order/";
+    }
+}
